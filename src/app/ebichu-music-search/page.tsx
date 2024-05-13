aab3517eed10180ed9e1352d223ad7db8998f062
misaki <misaki02984@gmail.com>
2024-05-13 21:51:50 +0900

    initialCommit

---
+++ src/app/ebichu-music-search/page.tsx
@@ -0,0 +1,11 @@
+export default function Page({children}: {children: React.ReactNode}) {
+    return (
+        // <section>
+        //     {children}
+        // </section>
+        <>
+        <p>aaaa</p>
+        <p>iii</p>
+        </>
+    );
+}
